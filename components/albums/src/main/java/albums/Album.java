55b62028e6962762bb536bc7074fe6e22f20fac3
kwangeumLee <kelee@lsis.com>
2019-08-27 10:23:00 +0900

    git add .

--- components/albums/src/main/java/albums/Album.java
+++ components/albums/src/main/java/albums/Album.java
@@ -1,4 +1,4 @@
-/**
+package albums; /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,23 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.superbiz.moviefun.movies;
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -38,51 +22,47 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
-public class Movie implements Serializable {
+public class Album implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    private String director;
+    private String artist;
     private String title;
     private int year;
-    private String genre;
     private int rating;
 
-    public Movie() {
+    public Album() {
     }
 
-    public Movie(String title, String director, String genre, int rating, int year) {
-        this.director = director;
+    public Album(String artist, String title, int year, int rating) {
+        this.artist = artist;
         this.title = title;
         this.year = year;
-        this.genre = genre;
         this.rating = rating;
     }
 
-    public Movie(String director, String title, int year) {
-        this.director = director;
-        this.title = title;
-        this.year = year;
-    }
-
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getDirector() {
-        return director;
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
     }
 
-    public void setDirector(String director) {
-        this.director = director;
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
     }
 
     public String getTitle() {
@@ -101,19 +81,28 @@ public class Movie implements Serializable {
         this.year = year;
     }
 
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
     public int getRating() {
         return rating;
     }
 
     public void setRating(int rating) {
         this.rating = rating;
+    }
+
+    public boolean hasId() {
+        return id != null;
+    }
+
+    public boolean isEquivalent(Album other) {
+        if (year != other.year) return false;
+        if (!isEqual(title, other.title)) return false;
+        if (!isEqual(artist, other.artist)) return false;
+
+        return true;
+    }
+
+    private static <T> boolean isEqual(T one, T other) {
+        if (one != null ? !one.equals(other) : other != null) return false;
+        return true;
     }
 }
